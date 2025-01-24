3e726a8b99f493e3c9b8c1176a186d209617e9b6
llurs <elurraa@gmail.com>
2025-01-25 00:13:46 +0800

    Weien changes #001
    - Added weien_testLevel
    - Added weien_PlayerController

---
+++ RainbowTiger/Assets/Scripts/weien/weien_PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weien_PlayerController : MonoBehaviour
+{
+    public float test = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
