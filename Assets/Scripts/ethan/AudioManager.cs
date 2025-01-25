59e2d1936747548fc42c723badac88e511bff38c
Rez3rk <jeanrayethan@gmail.com>
2025-01-26 03:04:18 +0800

    Added UI Assets And Beginnings Of Sprite Animations

--- Assets/Scripts/ethan/AudioManager.cs
+++ Assets/Scripts/ethan/AudioManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using JetBrains.Annotations;
 
 public class AudioManager : MonoBehaviour
 {
@@ -10,61 +11,111 @@ public class AudioManager : MonoBehaviour
     public Sound[] ostSounds, sfxSounds;
     public AudioSource ostSource, sfxSource;
 
+  public bool isOSTMuted = false;
+    public bool isSFXMuted = false;
+
     private void Awake()
     {
-        // Ensure there is only one instance of AudioManager
         if (Instance == null)
         {
-            Instance = this; // Set the static reference to this instance
+            Instance = this;
         }
         else
         {
             Debug.LogWarning("Duplicate AudioManager detected and destroyed.");
-            Destroy(gameObject); // Destroy duplicate instances
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            AudioManager.Instance.PlaySFX("SFX_Gum_Grapple");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            AudioManager.Instance.PlaySFX("SFX_Gum_Inflate");
         }
     }
 
     public void Start()
     {
-
         PlayOST("OST_Main");
     }
     public void PlayOST(string name)
     {
         Sound s = Array.Find(ostSounds, x => x.name == name);
 
-        if (s == null)
-        {
-            Debug.LogWarning("Sound Not Found: " + name);
-            return;
-        }
 
-        else
+        if (isOSTMuted == false)
         {
-            ostSource.clip = s.clip;
-            ostSource.volume = Mathf.Clamp(s.volume, 0f, 2f);
-            ostSource.pitch = s.pitch;
-            ostSource.Play();
+            {
+                if (s == null)
+                {
+                    Debug.LogWarning("Sound Not Found: " + name);
+                    return;
+                }
+
+                else
+                {
+                    ostSource.clip = s.clip;
+                    ostSource.volume = Mathf.Clamp(s.volume, 0f, 2f);
+                    ostSource.pitch = s.pitch;
+                    ostSource.Play();
+                }
+            }
         }
-        
+    }
+
+    public void StopOST()
+    {
+        if (ostSource.isPlaying)
+        {
+            ostSource.Stop();
+        }
     }
 
     public void PlaySFX(string name)
     {
         Sound s = Array.Find(sfxSounds, x => x.name == name);
-
-        if (s == null)
+        if (isSFXMuted == false)
         {
-            Debug.LogWarning("Sound Not Found: " + name);
-            return;
-        }
+            if (s == null)
+            {
+                Debug.LogWarning("Sound Not Found: " + name);
+                return;
+            }
 
+            else
+            {
+                sfxSource.pitch = s.pitch;
+                sfxSource.PlayOneShot(s.clip, s.volume);
+            }
+        }
+    }
+
+    public void MuteOST()
+    {
+        if (isOSTMuted)
+        {
+            isOSTMuted = false;
+            ostSource.Play();
+        }
         else
         {
-            sfxSource.pitch = s.pitch;
-            sfxSource.PlayOneShot(s.clip, s.volume);
+            // Mute and stop the OST
+            isOSTMuted = true;
+            if (ostSource.isPlaying)
+            {
+                ostSource.Stop();
+            }
         }
+    }
 
-        
+    public void MuteSFX()
+    {
+        isSFXMuted = !isSFXMuted;
     }
 }
